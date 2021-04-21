3e071ae353ed65c0cb6a3207ef0bf26944f6d422
filipetrinidad <fipe5766@gmail.com>
2021-04-21 11:21:58 -0300

    read date

--- app/src/main/java/database_firebase/com/MainActivity.java
+++ app/src/main/java/database_firebase/com/MainActivity.java
@@ -1,11 +1,16 @@
 package database_firebase.com;
 
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,15 +21,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        DatabaseReference usuarios = referencia.child( "usuarios" );
+        DatabaseReference usuarios = referencia.child("usuarios");
 
         Usuario usuario = new Usuario();
-        usuario.setName("Pedro");
-        usuario.setSobrenome("Guilherme");
+        usuario.setName("Filipe");
+        usuario.setSobrenome("Santos");
         usuario.setEmail("fipe5766@gmail.com");
         usuario.setSenha("1234");
 
-        usuarios.setValue( usuario );
+        usuarios.child("3").setValue(usuario);
+
+        usuarios.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Log.i("Firebase", snapshot.getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
     }
 }
